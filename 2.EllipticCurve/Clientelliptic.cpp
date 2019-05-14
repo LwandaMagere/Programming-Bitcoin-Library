52dca6901929af4230742caf6438ab087f5c0afd
Steven Dado <dadolenssen@gmail.com>
2019-05-14 14:14:15 +0300

    Eliptic curve

--- 2.EllipticCurve/Clientelliptic.cpp
+++ 2.EllipticCurve/Clientelliptic.cpp
@@ -8,12 +8,26 @@ class Point
     private:
         int x, y, a , b;
     public:
-    Point(int, int, int, int);
+    Point(int, int, int j = 0, int m = 7); // you give default here
     ~Point();
     bool operator==(Point & s);
+    Point operator+(Point & s);
+    friend ostream & operator <<(ostream & os, const Point & s);
         
         
 };
+
+ostream & operator <<(ostream & os, const Point & s)
+{
+	
+	os << s.x << ", " << s.y << endl;
+	return os;
+}
+
+ Point Point::operator+( Point & s)
+{
+        return Point(x + s.x, y + s.y); // default is implied
+}
 
 Point::Point(int c, int d, int f, int e)
 {
@@ -30,11 +44,14 @@ Point::Point(int c, int d, int f, int e)
     exponent = 3;
     answer2 = pow(base,exponent) + (a * x + b);
 
-    if (answer1 != answer2)
-        cerr << "The points are not in the curve";
+    if (answer1 != answer2)  // We check here that the point is actually on the curve.
+        cerr << "The points are not in the curve\n";
+    if (x == 0 && y == 0)
+        cout << "This is the point at infinity\n";
 }
-
- bool Point::operator==(Point & s)
+// Points are equal if and only if they are on the same curve and have the same
+//coordinates.
+ bool Point::operator==(Point & s) 
  {
      return x == s.x && y == s.y && a == s.a && b == s.b;
  }
@@ -44,8 +61,18 @@ Point::Point(int c, int d, int f, int e)
 
 int main()
 {
+    // We can now create Point objects, and we will get an error if the point is not on the
+    //curve:
     Point p1(-1, -1, 5, 7);
-    Point p2(-1, -2, 5, 7);
+    Point p2(-1, -2, 5, 7); 
+
+    Point p3(-1, -1, 5, 7);
+    Point p4(-1, 1, 5, 7);
+    Point inf(0, 0, 5, 7);
+
+    cout << p3 + inf << "\n";
+    cout << inf + p4 << "\n";
+    cout << p3 + p4 << "\n";
 
     return 0;
 }

d354d6f0e3223357d4f26e67b8c17f757be72575
Steven Dado <dadolenssen@gmail.com>
2019-05-27 16:58:19 +0300

    updates

--- 2.EllipticCurve/Clientelliptic.cpp
+++ 2.EllipticCurve/Clientelliptic.cpp
@@ -1,50 +1,20 @@
 #include <iostream>
 #include <cmath>
-#include "../FiniteField/finiteHeader.h"
 using namespace std;
 
 class Point
 {
     private:
         int x, y, a , b;
+
     public:
-    Point(int, int, int, int ); // you give default here
-  //  Point();
-    ~Point();
+    Point(int, int, int, int );
     bool operator==(Point & s);
     Point operator+(Point & s);
     friend ostream & operator <<(ostream & os, const Point & s);
-        
-        
+     ~Point();
+               
 };
-
-
-
-ostream & operator <<(ostream & os, const Point & s)
-{
-	
-	os << s.x << ", " << s.y << endl;
-	return os;
-}
-
- Point Point::operator+(Point & s)
-{
-         int base, exponent, k;
-        
-         if (x != s.x)
-         {
-              k = (y - s.y)/(x - s.x);
-              base = k;
-              exponent = 2;
-              x = pow(base, exponent) - x - s.x;
-              y = k * (x -s.x) - y;
-              return s;
-         }
-         int m, j;
-          
-        return Point(x + s.x, y + s.y, j = 0, m = 7); // default is implied
-      
-}
 
 Point::Point(int c, int d, int f, int e)
 {
@@ -61,11 +31,51 @@ Point::Point(int c, int d, int f, int e)
     exponent = 3;
     answer2 = pow(base,exponent) + (a * x + b);
 
+     if (x == 0 && y == 0)
+     {
+          cout << "This is the point at infinity\n";
+          return;
+     }
+       
+
     if (answer1 != answer2)  // We check here that the point is actually on the curve.
-        cerr << "The points are not in the curve\n";
-    if (x == 0 && y == 0)
-        cout << "This is the point at infinity\n";
+        cerr << "This points is not on the curve\n";
+    else
+    {
+        answer1 == answer2;
+        cout << "This point is on the curve\n ";
+    }
+       
+   
 }
+
+
+
+ostream & operator <<(ostream & os, const Point & s)
+{
+	
+	os << s.x << ", " << s.y << endl;
+	return os;
+}
+
+ Point Point::operator+(Point & s)
+{
+        
+         if (a != s.a || b != s.b)
+            cerr << "Points are not on the same curve\n ";
+         if (x == s.x && y == s.y && y == 0 * x)
+         {
+             cout << "Returning the point at infinity\n";
+             return *this;
+         }
+            
+         if ( x == 0)
+            return s;
+         if (s.x == 0)
+            return *this;      
+      
+}
+
 // Points are equal if and only if they are on the same curve and have the same
 //coordinates.
  bool Point::operator==(Point & s) 
@@ -75,14 +85,17 @@ Point::Point(int c, int d, int f, int e)
 
  Point::~Point(){}
 
-
 int main()
 {
     // We can now create Point objects, and we will get an error if the point is not on the
     //curve:
     Point p1(-1, -1, 5, 7);
-    Point p2(-1, -2, 5, 7); 
+    cout << p1 << "\n"; 
 
+    Point p2(-1, 2, 5, 7); 
+    cout << p2 << "\n";
+
+    
     Point p3(-1, -1, 5, 7);
     Point p4(-1, 1, 5, 7);
     Point inf(0, 0, 5, 7);
